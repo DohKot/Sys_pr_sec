c0d8674d735bad286233cb552b5fe56a31149f2f
sa <sa@astra>
2025-03-07 01:33:34 +0300

    first commit

---
+++ build/CMakeFiles/pr2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sa/Prk2/error_handler.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/error_handler.c.o"
+  "/home/sa/Prk2/help.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/help.c.o"
+  "/home/sa/Prk2/log.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/log.c.o"
+  "/home/sa/Prk2/main.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/main.c.o"
+  "/home/sa/Prk2/processes.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/processes.c.o"
+  "/home/sa/Prk2/users.c" "/home/sa/Prk2/build/CMakeFiles/pr2.dir/users.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pr2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pr2.dir/error_handler.c.o"
+  "CMakeFiles/pr2.dir/help.c.o"
+  "CMakeFiles/pr2.dir/log.c.o"
+  "CMakeFiles/pr2.dir/main.c.o"
+  "CMakeFiles/pr2.dir/processes.c.o"
+  "CMakeFiles/pr2.dir/users.c.o"
+  "pr2"
+  "pr2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pr2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
